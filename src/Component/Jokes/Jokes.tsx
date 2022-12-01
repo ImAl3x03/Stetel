8a89a3cdf630f43670b45fa4e88b1537919f6d3c
Alex Di Maria <alessandro.dimaria3@outlook.it>
2022-12-01 12:14:13 +0100

    feat: jokes hooks

--- src/Component/Jokes/Jokes.tsx
+++ src/Component/Jokes/Jokes.tsx
@@ -1,24 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import React from 'react';
 import css from "./jokes.module.css";
+import {useJokes} from "./useJokes";
 
 interface JokesProps {
     counter: number;
 }
 
-const url: string = "https://api.chucknorris.io/jokes/random";
-
 const Jokes: React.FC<JokesProps> = ({counter}) => {
-    const [jokes, setJokes] = useState<string>("");
-
-    useEffect(() => {
-        if (counter !== 0) {
-            void axios.get(url)
-                .then(({data}) => {
-                    setJokes(data.value);
-                });
-        }
-    }, [counter])
+    const jokes = useJokes(counter);
 
     return (
         <div className={css.jokes}>
